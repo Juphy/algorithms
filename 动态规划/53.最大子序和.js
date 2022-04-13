c8f8f9f6857aca5766e0b7732cbc6bb930f8255c
gaojuphy <1571066810@qq.com>
2022-04-13 23:32:30 +0800

    最大子序和复习

--- 动态规划/53.最大子序和.js
+++ 动态规划/53.最大子序和.js
@@ -1,10 +1,10 @@
-function maxSubArray(nums){
+function maxSubArray(nums) {
     let res = nums[0], sum = nums[0], len = nums.length
-    for(let i = 1; i < len; i++){
+    for (let i = 1; i < len; i++) {
         let cur = nums[i]
-        if(sum > 0){
+        if (sum > 0) {
             sum += cur
-        }else{
+        } else {
             sum = cur
         }
         res = Math.max(res, sum)
@@ -14,3 +14,47 @@ function maxSubArray(nums){
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 console.log(maxSubArray([1]))
 console.log(maxSubArray([5, 4, -1, 7, 8]))
+
+function maxSubArray(nums) {
+    let max = -Infinity, len = nums.length;
+    for (let i = 0; i < len; i++) {
+        let sum = 0;
+        for (let j = i; j < len; j++) {
+            sum += nums[j]
+            if (sum > max) max = sum
+        }
+    }
+    return max
+}
+
+function maxSubArray(nums) {
+    let len = nums.length, dp = [nums[0]], res = nums[0]
+    for (let i = 1; i < len; i++) {
+        dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+        res = Math.max(res, dp[i])
+    }
+    return res
+}
+
+function maxSubArray(nums) {
+    let len = nums.length, dp = nums[0], res = nums[0]
+    for (let i = 1; i < len; i++) {
+        dp = Math.max(dp + nums[i], nums[i])
+        res = Math.max(res, dp)
+    }
+    return res
+}
+
+function maxSubArray(nums){
+    let len = nums.length, max = nums[0], res = 0
+    for(let i = 0; i < len; i++){
+        let num = nums[i]
+        if(sum + num > num){
+            sum = sum + num
+        }else{
+            sum = num
+        }
+        res = Math.max(sum, res)
+    }
+    return res
+}
