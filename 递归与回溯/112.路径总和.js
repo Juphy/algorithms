904ed8cdd23b4b3536609559ca1a085990a11f9d
gaojuphy <1571066810@qq.com>
2022-04-13 23:55:23 +0800

    路径总和复习

--- 递归与回溯/112.路径总和.js
+++ 递归与回溯/112.路径总和.js
@@ -1,8 +1,4 @@
-function TreeNode(val, left, right) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-}
+import TreeNode from "../utils/TreeNode"
 function hasPathSum(root, targetSum) {
     if (root == null) return false
     if (root.left == null && root.right == null) return targetSum - root.val === 0
@@ -22,4 +18,24 @@ function hasPathSum(root, targetSum) {
     }
     dfs(root, 0)
     return hasPath
+}
+
+function hasPathSum(root, targetSum) {
+    let hasPath = false
+    function dfs(root, target) {
+        if (root == null) return
+        if (root.left == null && root.right == null && target === root.val) {
+            hasPath = true;
+        }
+        if (root.left) dfs(root.left, target - root.val)
+        if (root.right) dfs(root.right, target - root.val)
+    }
+    dfs(root, targetSum)
+    return hasPath
+}
+
+function hasPathSum(root, target) {
+    if (root == null) return false
+    if (root.left == null && root.right == null) return target === root.val
+    return hasPathSum(root.left, target - root.val) || hasPathSum(root.right, target - root.val)
 }
