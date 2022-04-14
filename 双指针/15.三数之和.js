9c3da2700fa32d2628078dbe007420acc7ba93ce
gaojuphy <1571066810@qq.com>
2022-04-14 23:09:22 +0800

    修改bug

--- 双指针/15.三数之和.js
+++ 双指针/15.三数之和.js
@@ -4,7 +4,7 @@ function threeSum(nums) {
     nums.sort((a, b) => a - b)
     for (let i = 0; i < nums.length - 2; i++) {
         if (nums[i] > 0) break;
-        if (k > 0 && nums[k] === nums[k-1]) continue
+        if (i > 0 && nums[i] === nums[i-1]) continue
         let left = i + 1, right = nums.length - 1
         while (left < right) {
             if (nums[i] + nums[left] + nums[right] > 0) {
