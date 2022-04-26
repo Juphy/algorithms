e94d77b20c7ad3affafe0c9e7c7704449df3ca99
gaojuphy <1571066810@qq.com>
2022-04-26 11:37:50 +0800

    快排

--- 排序算法/快速排序.js
+++ 排序算法/快速排序.js
@@ -17,9 +17,43 @@ function quickSort(arr){
     return quickSort(left).concat(arr[midInex], quickSort(right))
 }
 let nums = []
-for(let i = 100000; i >= 0; i--){
+for(let i = 1000000; i >= 0; i--){
     nums.push(i)
 }
 console.time('start')
 console.log(quickSort(nums))
 console.timeEnd('start')
+
+function quickSort1(nums){
+    function f(nums, start, end){
+        if(start >= end) return
+        let left = start, right = end
+          midVal = nums[Math.floor((start + end)/2)]
+        // 两边指针置换midVal两边的数值  
+        while(left <= right){
+            while(left <= right && nums[left] < midVal){
+                left++
+            }
+            while(left <= right && nums[right] > midVal){
+                right--
+            }
+            if(left <= right){
+                [nums[left], nums[right]] = [nums[right], nums[left]]
+                left++
+                right--
+            }
+        }
+        f(nums, start, right)
+        f(nums, left, end)  
+    }
+    f(nums, 0, nums.length - 1)
+    return nums
+}
+
+let nums1 = []
+for(let i = 1000000; i >= 0; i--){
+    nums1.push(i)
+}
+console.time('start1')
+console.log(quickSort1(nums1))
+console.timeEnd('start1')
