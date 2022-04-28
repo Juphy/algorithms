430b596e8267374f5b0b5041a11d75af81cda051
gaojuphy <1571066810@qq.com>
2022-04-29 02:14:16 +0800

    链表中倒数最后k个节点

---
+++ 双指针/链表中倒数最后k个节点.js
@@ -0,0 +1,16 @@
+function findKthList(head, k){
+    let fast = haed, slow = head
+    while(k > 0){
+        if(fast){
+            fast = fast.next
+        }else{
+            return null
+        }
+        k--
+    }
+    if(fast){
+        fast = fats.next
+        slow = slow.next
+    }
+    return slow
+}
