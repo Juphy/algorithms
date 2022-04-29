52777bb3886c789223f6f87eff246b1c5fffe991
gaojuphy <1571066810@qq.com>
2022-04-29 16:06:10 +0800

    回文链表

--- 链表相关/判断一个链表是否为回文结构.js
+++ 链表相关/判断一个链表是否为回文结构.js
@@ -17,3 +17,25 @@ function isPail( head ) {
     }
     return true
 }
+
+function reverseList(head){
+    if(head == null) return false
+    if(head.next == null) return true
+    let fast = head, slow = head, pre = null
+    while(fast&&fast.next){
+        fast = fast.next.next
+        let tmp = slow.next
+        slow.next = pre
+        pre = slow
+        slow = tmp
+    }
+    if(fast) slow = slow.next
+    while(pre&&slow){
+        if(pre.val !== slow.val){
+            return false
+        }
+        pre = pre.next
+        slow = slow.next
+    }
+    return true
+}
