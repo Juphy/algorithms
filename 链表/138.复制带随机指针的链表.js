d659eb9e78435b71b4c21ee8d240a61757eb5dc7
gaojuphy <1571066810@qq.com>
2022-05-04 18:03:01 +0800

    修改文件夹名称

---
+++ 链表/138.复制带随机指针的链表.js
@@ -0,0 +1,15 @@
+var copyRandomList = function(head) {
+    if(!head) return head
+    let cur = head, map = new Map()
+    while(cur){
+        map.set(cur, new Node(cur.val))
+        cur = cur.next
+    }
+    cur = head
+    while(cur){
+        map.get(cur).next = map.get(cur.next) || null
+        map.get(cur).random = map.get(cur.random) || null
+        cur = cur.next
+    }
+    return map.get(head)
+};
